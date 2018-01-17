5c94a0364710358667a43c9b862d12c283c9f935
Roy Lovitt <lovittl@my.erau.edu>
2018-01-16 23:30:03 -0500

    Add project files.

---
+++ Mod2Ex2A/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod2Ex2A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter 10 Integers as Prompted");
+/* construct a loop that Prompts the user for the first input and directs user to hit enter until done.
+ * loop will begin with an initial value of 0 in a variable to hold the sum
+ * int sum = 0
+ *the loop incremented and the user prompted to input Num2 and so on until num10 is read 
+ * Increment the index and then read the value of next NumX until Num9 has been read and added to sum.
+ *  1
+                   do while i =< 10 i++
+                   sum = sum + NextNum
+                   Console.writeline{enter num(${i})}
+                   
+            Console.writeline(${sum})
+         }
+*/
+
+    }
+    }
+}
