ca75affa47f55a58ae276116e33adc8db664cdb6
Roy Lovitt <lovittl@my.erau.edu>
2018-01-17 19:36:00 -0500

    completed code

--- Mod2Ex2A/Program.cs
+++ Mod2Ex2A/Program.cs
@@ -10,28 +10,26 @@ namespace Mod2Ex2A
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please Enter 10 Integers as Prompted");
-/* construct a loop that Prompts the user for the first input and directs user to hit enter until done.
- * loop will begin with an initial value of 0 in a variable to hold the sum
- * int sum = 0
- *the loop incremented and the user prompted to input Num2 and so on until num10 is read 
- * Increment the index and then read the value of next NumX until Num9 has been read and added to sum.
- *  1
-                   do while i =< 10 i++
-                   sum = sum + NextNum
-                   Console.writeline{enter num(${i})}
+          
+        int i,n,sum=0;
+	    	
+	    Console.Write("\n\n");
+        Console.Write("Read 10 numbers and calculate sum and average:\n");
+        Console.Write("----------------------------------------------");
+        Console.Write("\n\n");
+	
+	    Console.Write("Input the 10 numbers : \n");
 
-            int i = 0; while i < 9
-                for i = 0
-                get input 
-                    sum = sum + input
-                    i++
+            for (i=1;i<=10;i++)
+	        {
+            Console.Write("Number-{0} :",i);
+            n= Convert.ToInt32(Console.ReadLine());		
+		    sum +=n;
+	        }
 
-                   
-            Console.writeline(${sum})
-         }
-*/
+        Console.WriteLine((sum));
 
-    }
+        }
     }
 }
+
